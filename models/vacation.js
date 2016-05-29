3e829f05b128fce62ea1373374179f9dccedbb08
GLINRAEN <MISTRESS.WYN@GMAIL.COM>
2016-05-28 21:10:35 -0700

    added more routes

--- models/vacation.js
+++ models/vacation.js
@@ -2,6 +2,7 @@ var Schema       = mongoose.Schema;
 
 var vacationSchema   = new Schema({
     _id: Number,
+    name: String,
     country: String,
     date: Date,
     duration: String,
--- server.js
+++ server.js
@@ -30,10 +30,18 @@ app.get('/', function homepage(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+//get all vacas
 app.get("/vacation", function(req, res) {
+  Vacation.find(function(err, vacations) {
+  res.json("vacations");
+  });
 });
 
+//create a vaca
 app.post("/vacation", function(req, res) {
+  var newVacation = req.body.name;
+  vacation.push(newVacation);
+  res.send(name + " is created.");
 });
 
 /*  "/vacation:id"
@@ -41,16 +49,22 @@ app.post("/vacation", function(req, res) {
  *    PUT: update vacation by id
  *    DELETE: deletes vacation by id
  */
-
+//get one vaca
 app.get("/vacation/:id", function(req, res) {
+  var vacation = vacation[req.param.id];
+  res.json(vacation);
 });
 
+//update a vaca
 app.put("/vacation/:id", function(req, res) {
+  res
 });
 
+//delete a vaca
 app.delete("/vacation/:id", function(req, res) {
+  quotes.splice(req.params.id, 1);
+  res.json(true);
 });
-
 
 /*
  * JSON API Endpoints
@@ -61,11 +75,13 @@ app.get('/api', function api_index(req, res) {
   res.json({
     message: "Welcome to my personal api! Here's what you need to know!",
     documentation_url: "https://github.com/example-username/express_self_api/README.md", // CHANGE ME
-    base_url: "http://vacation.herokuapp.com", // CHANGE ME
+    base_url: "peaceful-cliffs-50130.herokuapp.com", // CHANGE ME
+    description: "Returns all vacations that are avaiable to access.",
+
     endpoints: [
       {method: "GET", path: "/api", description: "Describes all available endpoints"},
       {method: "GET", path: "/api/profile", description: "About me"}, // CHANGE ME
-      {method: "POST", path: "/api/campsites", description: "Create a new campsite"} // CHANGE ME
+      {method: "POST", path: "/api/vacation", description: "Create a new vacation"} // CHANGE ME
     ]
   });
 });
